fc70eeb7a0c2dd7ce856a3b4f4a841b1292aa2a8
Kayla Robinson <kaylarobinson077@gmail.com>
2022-07-25 16:40:41 -0500

    make abstract and image look prettier

--- pages/Publications.py
+++ pages/Publications.py
@@ -13,22 +13,20 @@ px = unit.px
 rem = unit.rem
 em = unit.em
 
-keywords_dict = {
+TAG_COLORS = {
     "data science": "#ff4b4b",
     "medical physics": "#ffa421",
     "ml ops": "#ffe312",
     "insurance": "#21c354",
+    "digital breast tomosynthesis": "#00d4b1", 
+    "mammography": "#00c0f2",
+    "deep learning": "#1c83e1",
+    "convolutional neural networks": "#803df5",
+    "computer-aided diagnosis": "#808495",
 }
 
 # Colors from the Streamlit palette.
 # These are red-70, orange-70, ..., violet-70, gray-70.
-PALETTE = [
-    "#00d4b1",
-    "#00c0f2",
-    "#1c83e1",
-    "#803df5",
-    "#808495",
-]
 
 OPACITIES = [
     "33", "66",
@@ -43,12 +41,12 @@ def render_tag(tag: str, color: str):
     if color:
         color_style['color'] = color
 
-    background = None
-    if not background:
-        label_sum = sum(ord(c) for c in tag)
-        background_color = PALETTE[label_sum % len(PALETTE)]
-        background_opacity = OPACITIES[label_sum % len(OPACITIES)]
-        background = background_color + background_opacity
+    # background = None
+    # if not background:
+    # label_sum = sum(ord(c) for c in tag)
+    background_color = TAG_COLORS[tag]
+    background_opacity = "33"
+    background = background_color + background_opacity
         
     return (
         span(
@@ -80,6 +78,7 @@ def render_tags(tags: list) -> str:
         color = "black"
         tag_html = render_tag(tag, color)
         out(tag_html)
+        out(html.escape(" "))
 
     tag_string = str(out)
     st.markdown(
@@ -89,16 +88,17 @@ def render_tags(tags: list) -> str:
 
     
 def publication_element(pub_data: dict):
-
-    st.markdown(f'### [{pub_data["title"]}]({pub_data["link"]})')
     
     col1, col2 = st.columns(2)
     with col1:
+        st.markdown(f'**[{pub_data["title"]}]({pub_data["link"]})**')
         st.markdown(pub_data["authors"])
         st.markdown(pub_data["journal"])
         st.markdown(pub_data["date"])
-        st.markdown(pub_data["type"])
+        # st.markdown(pub_data["type"])
         render_tags(pub_data["tags"])
+        with st.expander("Abstract"):
+            st.write(pub_data["abstract"])
     with col2:
         st.image(pub_data["image"])
     
